b2f0bfa1912256a64750eb9c341eea44ee8274fa
Kazagha <chrisg.local@gmail.com>
2015-04-24 10:59:26 +1000

    Function: Initial Commit. This function is used for the CoderByte challenge. http://coderbyte.com/

---
+++ src/Codebyte/Function.java
@@ -0,0 +1,58 @@
+package Codebyte;
+
+import java.util.*; 
+import java.io.*;
+
+class Function {  
+  String FirstReverse(String input) { 
+    String output = "";
+	for( int i = 0; i < input.length(); i++ ){
+		output = output + input.charAt(input.length() - i - 1);
+	}
+	return output;
+  } 
+  
+  String LetterChanges(String str) { 
+	  
+	    // code goes here   
+	    /* Note: In Java the return type of a function and the 
+	       parameter types being passed are defined, so this return 
+	       call must match the return type of the function.
+	       You are free to modify the return type. */
+	    
+	  	//System.out.println(Character.getNumericValue(str.charAt(0)));
+	  
+	  String temp = new String("");
+	  
+		  for(int i = 0; i < str.length(); i++){
+			  temp += convert(str.charAt(i));
+		  }
+		  
+	    return temp;
+	    
+	  } 
+  
+  char convert(char c){	  
+	  int temp = Character.getNumericValue(c) + 1;
+	  return Character.forDigit(temp, 36);
+  }
+  
+  String FirstFactorial(String s) {
+	  int num = Integer.valueOf(s);
+	  int temp = 1;
+	  
+	  for(int i = 1; i <= num; i++){
+		  temp = temp * i;
+	  }
+	  
+	  return String.valueOf(temp);
+  }
+  
+  public static void main (String[] args) {  
+	    // keep this function call here     
+	  Scanner  s = new Scanner(System.in);
+	    Function c = new Function();
+	    System.out.print(c.FirstFactorial(s.nextLine())); 
+  }   
+  
+}    
