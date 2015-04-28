26028852c438995291ae5c6c4bbb6105eec58867
Chris.Green <Chris.Green@DACSC-N01256.wdrc.local>
2015-04-28 13:04:04 +1000

    Function: Fix indent spacing, add comments.

--- src/Codebyte/Function.java
+++ src/Codebyte/Function.java
@@ -4,71 +4,86 @@ import java.util.*;
 import java.io.*;
 
 class Function {  
-  String FirstReverse(String input) { 
-    String output = "";
-	for( int i = 0; i < input.length(); i++ ){
-		output = output + input.charAt(input.length() - i - 1);
+	
+	/**
+	 * Reverse the specified input string
+	 * @param input
+	 * @return
+	 */
+	String FirstReverse(String input) { 
+		String output = "";
+		for( int i = 0; i < input.length(); i++ ){
+			output = output + input.charAt(input.length() - i - 1);
+		}
+		return output;
+	}   
+
+	/**
+	 * Change the all the letters in the input string to the next letter in the alphabet
+	 * @param str
+	 * @return
+	 */
+	String LetterChanges(String str) {	  
+		String temp = new String("");
+	  
+	  	for(int i = 0; i < str.length(); i++){
+	  		temp += nextLetter(str.charAt(i));
+	  	}
+	  	
+	  	return temp;	  	
+	} 
+  
+	/**
+	 * Using the input char, return the next letter in the alphabet
+	 * @param c
+	 * @return
+	 */
+	char nextLetter(char c){	  
+		int temp = Character.getNumericValue(c) + 1;
+		return Character.forDigit(temp, 36);
 	}
-	return output;
-  } 
   
-  String LetterChanges(String str) { 
+	/**
+	 * Find the factorial of the numbers in the input string. 
+	 * @param s
+	 * @return
+	 */
+	String FirstFactorial(String s) {
+		int num = Integer.valueOf(s);
+		int temp = 1;
+		
+		for(int i = 1; i <= num; i++){
+			temp = temp * i;
+		}
 	  
-	    // code goes here   
-	    /* Note: In Java the return type of a function and the 
-	       parameter types being passed are defined, so this return 
-	       call must match the return type of the function.
-	       You are free to modify the return type. */
-	    
-	  	//System.out.println(Character.getNumericValue(str.charAt(0)));
-	  
-	  String temp = new String("");
-	  
-		  for(int i = 0; i < str.length(); i++){
-			  temp += convert(str.charAt(i));
-		  }
-		  
-	    return temp;
-	    
-	  } 
+		return String.valueOf(temp);
+	}
   
-  char convert(char c){	  
-	  int temp = Character.getNumericValue(c) + 1;
-	  return Character.forDigit(temp, 36);
-  }
+	/**
+	 * Return the longest word in the string ignoring punctuation
+	 * @param sen
+	 * @return
+	 */
+	String LongestWord(String sen) {
+		// Leave only alphanumerical characters
+		sen = sen.replaceAll("[^A-Za-z0-9 ]", "");
+		
+		String[] parts = sen.split(" ");
+		String temp = new String("");
+	  
+		for(String s : parts){
+			if(s.length() > temp.length()){
+				temp = s;
+			}
+		}
+		
+		return temp;
+	}
   
-  String FirstFactorial(String s) {
-	  int num = Integer.valueOf(s);
-	  int temp = 1;
-	  
-	  for(int i = 1; i <= num; i++){
-		  temp = temp * i;
-	  }
-	  
-	  return String.valueOf(temp);
-  }
-  
-  String LongestWord(String sen) {
-	  // Leave only alpha
-	  sen = sen.replaceAll("[^A-Za-z0-9 ]", "");
-	  
-	  String[] parts = sen.split(" ");
-	  String temp = new String("");
-	  
-	  for(String s : parts){
-		  if(s.length() > temp.length()){
-			  temp = s;
-		  }
-	  }
-	  
-	  return temp;
-  }
-  
-  public static void main (String[] args) {  
-	    // keep this function call here     
-	  Scanner  s = new Scanner(System.in);
-	    Function c = new Function();
-	    System.out.print(c.LongestWord(s.nextLine())); 
-  }   
-  
+	public static void main (String[] args) {  
+		// keep this function call here     
+		Scanner  s = new Scanner(System.in);
+		Function c = new Function();
+		System.out.print(c.LetterChanges(s.nextLine())); 
+	}  
 }    
