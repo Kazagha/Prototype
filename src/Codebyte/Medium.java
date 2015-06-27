05462e745f2dc1f7bc2fe19f374b058d103995ec
Kazagha <chrisg.local@gmail.com>
2015-06-28 09:07:27 +1000

    Medium: Add the 'RunLength' method. Returns a compressed version of the String using the Run-Length encoding algorithm.

--- src/Codebyte/Medium.java
+++ src/Codebyte/Medium.java
@@ -33,10 +33,45 @@ public class Medium {
 		return String.valueOf(count);
 	}
 	
+	/**
+	 * Return a compressed version of the string using the Run-length encoding algorithm
+	 * For example "wwwggopp" would return 3w2g1o2p
+	 * @param str
+	 * @return
+	 */
+	String  RunLength(String str) {
+		String encStr = "";
+		// Set the previous char to the first char in the array
+		char prevChar = str.charAt(0);
+		// Offset by starting the count at zero
+		int count = 0;
+		
+		// Iterate throgh the chars
+		for(char c : str.toCharArray()) {
+			
+			if(c == prevChar) {
+				// If this char matches the prevChar add to the counter
+				count ++;			
+			} else {		
+				// Otherwise append to the encoded String
+				encStr += String.valueOf(count) + String.valueOf(prevChar);				
+				count = 1;
+			}
+			
+			// Reset the previous char
+			prevChar = c;
+		}
+		
+		// Append the final char to the encoded String
+		encStr += String.valueOf(count) + String.valueOf(prevChar);
+		
+		return encStr;
+	}
+	
 	public static void main (String[] args) {  
 		// keep this function call here     
 		Scanner  s = new Scanner(System.in);
 		Medium c = new Medium();
-		System.out.println(c.BinaryConverter(s.nextLine()));
+		System.out.println(c. RunLength(s.nextLine()));
 	} 
 }
