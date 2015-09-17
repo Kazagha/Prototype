3bd8754adfe3c0bd57c3f384b6c442b7d9d41b0c
Kazagha <chrisg.local@gmail.com>
2015-09-17 18:03:24 +1000

    JAXBController: Set the JAXB Marshaller for the sub class

--- src/InheritanceXML/JAXBController.java
+++ src/InheritanceXML/JAXBController.java
@@ -16,15 +16,16 @@ public class JAXBController
 	
 	public static void main (String[] args) throws JAXBException 
 	{
-		SubObject sub = new SubObject("Test", 10);
 		Basic basic = new Basic("Name", 12);
+		BaseObject base = new BaseObject("Base Object");
+		SubObject sub = new SubObject("Test", 10);		
 		
-		JAXBContext jaxbContext = JAXBContext.newInstance(Basic.class);		
+		JAXBContext jaxbContext = JAXBContext.newInstance(SubObject.class);		
 		Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
 		Marshaller marshaller = jaxbContext.createMarshaller();
 		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 		
-		marshaller.marshal(basic, System.out);	
+		marshaller.marshal(sub, System.out);	
 		
 	}
 }
