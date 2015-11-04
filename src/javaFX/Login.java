4015e2d80481ea9f11e85638893e34205da4ce0e
Kazagha <chrisg.local@gmail.com>
2015-11-04 12:51:46 +1000

    Login: Basic JavaFX login dialog

---
+++ src/javaFX/Login.java
@@ -0,0 +1,20 @@
+package javaFX;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Login extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception 
+	{
+		primaryStage.setTitle("JaxaFX Login");
+
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
+}
