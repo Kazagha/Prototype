e20e5a7ce1c4360df5d51239e8a0f95349d97a57
Kazagha <chrisg.local@gmail.com>
2015-11-04 12:55:46 +1000

    Login: Add basic grid pane layout

--- src/javaFX/Login.java
+++ src/javaFX/Login.java
@@ -1,6 +1,10 @@
 package javaFX;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Login extends Application {
@@ -9,7 +13,23 @@ public class Login extends Application {
 	public void start(Stage primaryStage) throws Exception 
 	{
 		primaryStage.setTitle("JaxaFX Login");
-
+		
+		// Create new grid layout
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.CENTER);
+		
+		// Set horizontal and vertical spacing
+		grid.setHgap(10);
+		grid.setVgap(10);
+				
+		grid.setPadding(new Insets(25, 25, 25, 25));
+		//grid.setGridLinesVisible(true);
+		
+		// Create the scene
+		Scene scene = new Scene(grid, 300, 275);
+		
+		// Add Scene and display
+		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 	
