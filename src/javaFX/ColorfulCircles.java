f1e0d354fc07bf9116869bc9c2d0ff4807cc41d0
Kazagha <chrisg.local@gmail.com>
2015-11-05 20:55:01 +1000

    ColorfulCircles: Add circles

--- src/javaFX/ColorfulCircles.java
+++ src/javaFX/ColorfulCircles.java
@@ -2,7 +2,10 @@ package javaFX;
 
 import javafx.application.Application;
 import javafx.scene.Group;
+import javafx.scene.effect.BoxBlur;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.StrokeType;
 import javafx.scene.*;
 import javafx.stage.Stage;
 
@@ -11,10 +14,30 @@ public class ColorfulCircles extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception 
 	{
+		// Setup
 		Group root = new Group();
 		Scene scene = new Scene(root, 800, 600, Color.BLACK);
 		primaryStage.setScene(scene);
 		
+		// Create Circles
+		Group circles = new Group();
+		for (int i = 0; i < 30; i++) 
+		{
+			Circle circle = new Circle(150, Color.web("white", 0.05));
+			circle.setStrokeType(StrokeType.OUTSIDE);
+			circle.setStroke(Color.web("white", 0.16));
+			circle.setStrokeWidth(4);
+			
+			circles.getChildren().add(circle);
+		}
+		
+		// Add Circles to the root group
+		root.getChildren().add(circles);
+		
+		// Add blur effect to circles
+		circles.setEffect(new BoxBlur(10, 10, 3));		
+		
+		// Display the GUI
 		primaryStage.show();
 	}
 	
