715708bb4a9c11542da046f66ce7c91d17ee206b
Kazagha <chrisg.local@gmail.com>
2015-11-06 15:05:50 +1000

    Controller: Initial commit
    Beginning of MVC

---
+++ src/javaFX/Controller.java
@@ -0,0 +1,24 @@
+package javaFX;
+
+import javafx.stage.Stage;
+
+public class Controller {
+	int count;
+	View view;
+	
+	public Controller(View view)
+	{
+		this.view = view;
+		this.count = 0;
+		
+		view.setAttempts(count);
+	}
+	
+	public static void main(String[] args) throws Exception
+	{
+		View v = new View();
+		Controller c = new Controller(v); 
+		
+		v.launch(LoginCSS.class);	
+	}	
+}
