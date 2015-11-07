bf5eecf9869e8474ffbed571cce8c77ec1d4fde1
Kazagha <chrisg.local@gmail.com>
2015-11-07 10:53:07 +1000

    Controller: Extend Application, implement EventHandler
    Move the 'Application' launch/start over to the controller and add the
    ability to handle events

--- src/javaFX/Controller.java
+++ src/javaFX/Controller.java
@@ -1,24 +1,42 @@
 package javaFX;
 
+import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-public class Controller {
+public class Controller extends Application 
+		implements EventHandler<Event> {
 	int count;
 	View view;
 	
-	public Controller(View view)
+	public Controller()
 	{
-		this.view = view;
-		this.count = 0;
-		
-		view.setAttempts(count);
+		// Create the model
+		count = 0;
+		// Create the view
+		view = new View();		
 	}
 	
 	public static void main(String[] args) throws Exception
+	{	
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// Set the scene, add the view
+		primaryStage.setTitle("MVC Test Application");
+		primaryStage.setScene(view.getScene());
+		//primaryStage.setScene(new Scene(new GridPane(), 250, 250));
+		primaryStage.show();		
+	}
+
+	@Override
+	public void handle(Event evt) 
 	{
-		View v = new View();
-		Controller c = new Controller(v); 
-		
-		v.launch(View.class);	
+		System.out.format("Event%n");
 	}	
 }
