7fcd20c4b1da2164fbcc57610feb0f53397aefc4
Kazagha <chrisg.local@gmail.com>
2015-11-12 20:25:33 +1000

    SlidePane: Setup basic Scene

--- src/javaFX/SlidePane.java
+++ src/javaFX/SlidePane.java
@@ -1,12 +1,32 @@
 package javaFX;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class SlidePane extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception 
+	{
+		VBox root = new VBox();
+		root.setAlignment(Pos.CENTER);
+		root.autosize();
+		
+		Scene scene = new Scene(root);
+		primaryStage.setTitle("Slide Test");
+		primaryStage.setWidth(350);
+		primaryStage.setHeight(350);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+		configureBox(root);
+	}
+	
+	public void configureBox(Node root)
 	{
 		
 	}
