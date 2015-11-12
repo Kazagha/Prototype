026fa88dc9d1a64ea4ca6d82482e2f3657d59e21
Kazagha <chrisg.local@gmail.com>
2015-11-12 21:22:03 +1000

    SlidePane: Animate close
    Both the left and right panels close into the middle when the 'close'
    button is selected

--- src/javaFX/SlidePane.java
+++ src/javaFX/SlidePane.java
@@ -1,6 +1,11 @@
 package javaFX;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Node;
@@ -15,10 +20,16 @@ import javafx.scene.paint.CycleMethod;
 import javafx.scene.paint.LinearGradient;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class SlidePane extends Application {
 	
 	private Rectangle bounds = new Rectangle(150, 150, 300, 300);
+	
+	private StackPane lPane;
+	private StackPane rPane;
+	private Timeline timelineLClose;
+	private Timeline timelineRClose;
 
 	@Override
 	public void start(Stage primaryStage) throws Exception 
@@ -44,19 +55,48 @@ public class SlidePane extends Application {
 		container.setPrefSize(bounds.getWidth(), bounds.getHeight());
 		container.setStyle("-fx-border-color: GREEN");
 		
-		StackPane lPane = new StackPane();
+		lPane = new StackPane();
 		lPane.getChildren().add(new Rectangle(bounds.getWidth(), bounds.getHeight(), Color.GREEN));
 		lPane.getChildren().add(new Label("Left Pane"));
 		
-		StackPane rPane = new StackPane();
+		rPane = new StackPane();
 		rPane.getChildren().add(new Rectangle(bounds.getWidth(), bounds.getHeight(), Color.ORANGE));
 		rPane.getChildren().add(new Label("Right Pane"));
 		
 		container.getChildren().addAll(lPane, rPane);
 		
+		// Setup the panel animation
+		setAnimation();
+		
 		Group gp = new Group();
 		gp.getChildren().add(container);
 		root.getChildren().addAll(controlPane(), gp);
+	}
+	
+	private void setAnimation()
+	{
+		// Set the starting position
+		lPane.translateXProperty().set(-bounds.getWidth());
+		rPane.translateXProperty().set(bounds.getWidth());
+		
+		timelineLClose = new Timeline();
+		timelineRClose = new Timeline();
+		
+		timelineLClose.setCycleCount(1);
+		timelineLClose.setAutoReverse(true);
+		final KeyValue kvLClose1;
+		final KeyValue kvLClose2;
+		final KeyValue kvLClose3 = new KeyValue(lPane.translateXProperty(), 0);
+		final KeyFrame kfLClose = new KeyFrame(Duration.millis(500), kvLClose3);
+		timelineLClose.getKeyFrames().add(kfLClose);
+		
+		timelineRClose.setCycleCount(1);
+		timelineRClose.setAutoReverse(true);
+		final KeyValue kvRClose1;
+		final KeyValue kvRClose2;
+		final KeyValue kvRClose3 = new KeyValue(rPane.translateXProperty(), 0);
+		final KeyFrame kfRClose = new KeyFrame(Duration.millis(500), kvRClose3);
+		timelineRClose.getKeyFrames().add(kfRClose);
 	}
 	
 	public HBox controlPane()
@@ -68,6 +108,14 @@ public class SlidePane extends Application {
 		
 		Button openButton = new Button("Open");
 		Button closeButton = new Button("Close");
+		
+		closeButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				timelineLClose.play();
+				timelineRClose.play();
+			}			
+		});
 		
 		hb.getChildren().addAll(openButton, closeButton);
 		
