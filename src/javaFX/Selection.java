e932c656bdf3f29d718f3a0f1d83549825810158
Kazagha <chrisg.local@gmail.com>
2015-11-17 12:50:02 +1000

    Selection: Create dummy data

--- src/javaFX/Selection.java
+++ src/javaFX/Selection.java
@@ -1,15 +1,24 @@
 package javaFX;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Selection extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		StackPane root = new StackPane();
+		VBox root = new VBox();
+		root.setAlignment(Pos.CENTER);
+		root.setSpacing(10);
+		
+		init(root);
 		
 		Scene scene = new Scene(root);
 		primaryStage.setTitle("Selection Test");
@@ -17,6 +26,21 @@ public class Selection extends Application {
 		primaryStage.setHeight(500);
 		primaryStage.setScene(scene);
 		primaryStage.show();
+	}
+	
+	public void init(VBox root)
+	{
+		for (int i = 0; i < 10; i++)
+		{			
+			GridPane grid = new GridPane();
+			grid.setAlignment(Pos.CENTER);
+			grid.add(new Label("Stack " + i + " "), 0, 0, 2, 1);
+			ProgressBar p = new ProgressBar(100);
+			p.setProgress(Math.random());
+			grid.add(p, 0, 1, 5, 1);
+			
+			root.getChildren().add(grid);
+		}
 	}
 	
 	public static void main(String[] args)
