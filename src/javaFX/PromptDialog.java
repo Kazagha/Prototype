f04fe95c93c53816d16f1f0239423aff0b2d978f
Kazagha <chrisg.local@gmail.com>
2015-11-22 10:08:38 +1000

    PromptDialog: Add basic dialog
    Basic prompt using GridPane for the layout of components

--- src/javaFX/PromptDialog.java
+++ src/javaFX/PromptDialog.java
@@ -1,17 +1,23 @@
 package javaFX;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class PromptDialog extends Application{
+	
+	String itemName = new String("Item Name");
+	int itemNum = 0;
 
 	@Override
 	public void start(Stage primaryStage) throws Exception 
@@ -34,7 +40,30 @@ public class PromptDialog extends Application{
 	
 	public void init(VBox root)
 	{
-		root.getChildren().add(new Label("Select"));		
+		Button edit = new Button("Edit");
+		edit.setOnAction(evt -> this.edit());	
+		root.getChildren().addAll(new Label("Edit Item"), edit);
+	}
+	
+	public void edit()
+	{
+		GridPane grid = new GridPane();
+		grid.setVgap(10);
+		grid.setHgap(10);
+		
+		TextField name  = new TextField(itemName.toString());
+		TextField number = new TextField(String.valueOf(itemNum));
+		
+		grid.add(new Label("Name"), 0, 0);
+		grid.add(name, 1, 0);
+		grid.add(new Label("Number"), 0, 1);
+		grid.add(number, 1, 1);
+		
+		Dialog d = new Dialog();
+		d.setTitle("Edit Item");
+		d.getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
+		d.getDialogPane().setContent(grid);
+		d.showAndWait();
 	}
 
 	public void showDialog() {		
@@ -42,5 +71,10 @@ public class PromptDialog extends Application{
 		d.getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
 		d.getDialogPane().setContent(new TextField("Test"));
 		d.showAndWait();
+	}
+	
+	public static void main(String[] args)
+	{
+		launch(args);
 	}
 }
